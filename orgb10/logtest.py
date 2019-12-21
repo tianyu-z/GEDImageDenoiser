9372b3fec6106a872aa647c6b64503f7726fdafb
agd <gauriaddepalli@gmail.com>
2019-12-21 04:49:58 -0500

    Add files via upload

---
+++ orgb10/logtest.py
@@ -0,0 +1,18 @@
+Loading model ...
+
+Loading data info ...
+
+data/Set12/01.png PSNR 31.334126
+data/Set12/02.png PSNR 34.216475
+data/Set12/03.png PSNR 32.065078
+data/Set12/04.png PSNR 31.427991
+data/Set12/05.png PSNR 32.370990
+data/Set12/06.png PSNR 31.055828
+data/Set12/07.png PSNR 30.796988
+data/Set12/08.png PSNR 33.913026
+data/Set12/09.png PSNR 30.022494
+data/Set12/10.png PSNR 31.684548
+data/Set12/11.png PSNR 31.812669
+data/Set12/12.png PSNR 31.706383
+
+PSNR on test data 31.867216
