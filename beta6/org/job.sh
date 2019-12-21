3bba261b602f600808952d99f411403ed3a3b5f9
agd <gauriaddepalli@gmail.com>
2019-12-21 04:45:50 -0500

    Add files via upload

---
+++ beta6/org/job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:p1080:1
+#SBATCH --time=80:00:00
+#SBATCH --mem=40GB
+#SBATCH --job-name=gowri
+#SBATCH --mail-type=END
+#SBATCH --mail-user=sga297@nyu.edu
+#SBATCH --output=gowri.out
+
+
+
+source ../../../py3.6.3/bin/activate
+python -u train.py --beta 6.0>>log3.txt
