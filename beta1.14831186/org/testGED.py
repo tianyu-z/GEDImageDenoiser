830635d8fb489f790d3f9588c9ae845f1cc7567f
agd <gauriaddepalli@gmail.com>
2019-12-21 04:24:21 -0500

    Add files via upload

---
+++ beta1.14831186/org/testGED.py
@@ -0,0 +1,80 @@
+import cv2
+import os
+import argparse
+import glob
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from models import DnCNN
+from utils import *
+from scipy.stats import gennorm
+from math import gamma
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+parser = argparse.ArgumentParser(description="DnCNN_Test")
+parser.add_argument("--num_of_layers", type=int, default=17, help="Number of total layers")
+parser.add_argument("--logdir", type=str, default="logs", help='path of log files')
+parser.add_argument("--test_data", type=str, default='Set68', help='test on Set12 or Set68')
+parser.add_argument("--test_noiseL", type=float, default=25, help='noise level used on test set')
+parser.add_argument("--beta", type=float, default=1.14831186, help='beta of GED')
+opt = parser.parse_args()
+
+def normalize(data):
+    return data/255.
+
+def getSize(noise):
+    k = 1
+    for i in range(len(noise.size())):
+        k *= noise.size()[i]
+    return k
+
+def getAlpha(std, beta):
+    return np.sqrt(np.square(std)/(gamma(3/beta)/gamma(1/beta)))
+    
+def main():
+    # Build model
+    print('Loading model ...\n')
+    net = DnCNN(channels=1, num_of_layers=opt.num_of_layers)
+    device_ids = [0]
+    model = nn.DataParallel(net, device_ids=device_ids).cuda()
+    model.load_state_dict(torch.load(os.path.join(opt.logdir, 'model_Best.pth')))
+    model.eval()
+    # load data info
+    print('Loading data info ...\n')
+    files_source = glob.glob(os.path.join('data', opt.test_data, '*.png'))
+    files_source.sort()
+    # process data
+    psnr_test = 0
+    for f in files_source:
+        # image
+        Img = cv2.imread(f)
+        Img = normalize(np.float32(Img[:,:,0]))
+        Img = np.expand_dims(Img, 0)
+        Img = np.expand_dims(Img, 1)
+        ISource = torch.Tensor(Img)
+        # noise
+        #noise = torch.FloatTensor(ISource.size()).uniform_(-1.732*opt.test_noiseL/255.,1.732*opt.test_noiseL/255.)
+        flatSize = getSize(ISource)
+        alpha = getAlpha(opt.test_noiseL/225.,opt.beta)
+        noise = torch.FloatTensor(gennorm.rvs(opt.beta, scale = alpha, size = flatSize, random_state = None))
+        noise = noise.view(ISource.size())
+        # noisy image
+        INoisy = ISource + noise
+        ISource, INoisy = Variable(ISource.cuda()), Variable(INoisy.cuda())
+        with torch.no_grad(): # this can save much memory
+            Out = torch.clamp(INoisy-model(INoisy), 0., 1.)
+        ## if you are using older version of PyTorch, torch.no_grad() may not be supported
+        # ISource, INoisy = Variable(ISource.cuda(),volatile=True), Variable(INoisy.cuda(),volatile=True)
+        # Out = torch.clamp(INoisy-model(INoisy), 0., 1.)
+        psnr = batch_PSNR(Out, ISource, 1.)
+        psnr_test += psnr
+        print("%s PSNR %f" % (f, psnr))
+    psnr_test /= len(files_source)
+    print("\nPSNR on test data %f" % psnr_test)
+
+if __name__ == "__main__":
+    main()
+
